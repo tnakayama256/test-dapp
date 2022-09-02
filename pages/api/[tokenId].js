fccdd17599a9dfdc563105d1f7020ecafd5bdcd4
takafumi <tnakayama256@gmail.com>
2022-09-02 20:03:19 +0900

    Initial commit

---
+++ pages/api/[tokenId].js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    const tokenId = req.query.tokenId
+    const image_url =
+        'https://raw.githubusercontent.com/tnakayama256/hotelnft/my-app/public/metadatas/'
+    res.status(200).json({
+        name: 'Hotel NFT #' + tokenId,
+        description: 'mktsherman',
+        image: image_url + 'hotel.png',
+    })
+}
